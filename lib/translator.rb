67e62d0a8a9bbbdc7f308c1192cb4df42087e327
Mehdi A Noorani <noorani.mehdi@outlook.com>
2020-10-18 20:45:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -31,7 +31,7 @@ def get_japanese_emoticon(file_path_two, english_emoticon)
       
       if drawing == english_emoticon
         
-        return language
+        return library[emotion][:japanese]
         
       end
     end
